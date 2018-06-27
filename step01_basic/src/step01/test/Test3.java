b2993b6e35f51d7a5a8da9b2359fbe8c4bdceac7
hwanginbeom <rydn2002@gmail.com>
2018-06-27 16:53:42 +0900

    03Type

--- step01_basic/src/step01/test/Test3.java
+++ step01_basic/src/step01/test/Test3.java
@@ -1,28 +1,28 @@
-package step01.test;
-
-public class Test3 {
-
-	
-	String message;
-	int age;
-	
-	public Test() {}
-	public Test(String a, int b) {
-	//String message; //선언해서 String 빼라
-		message = a;
-		age = b;
-		//객체 생성 없이 message, age 사용 가능한 방법이 없다
-	}
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		// TODO Auto-generated method stub  객체는 instance
-		Test t = new Test("오늘은 비가 오네요",46);
-		System.out.println(t.message);
-		System.out.println(t.age);
-
-	}
-
-}
+//package step01.test;
+//
+//public class Test3 {
+//
+//	
+//	String message;
+//	int age;
+//	
+//	public Test() {}
+//	public Test(String a, int b) {
+//	//String message; //선언해서 String 빼라
+//		message = a;
+//		age = b;
+//		//객체 생성 없이 message, age 사용 가능한 방법이 없다
+//	}
+//	
+//	
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//		// TODO Auto-generated method stub  객체는 instance
+//		Test t = new Test("오늘은 비가 오네요",46);
+//		System.out.println(t.message);
+//		System.out.println(t.age);
+//
+//	}
+//
+//}
