e0b7e0f4b2f586646edba1c5fc9244b59ad334b9
hwanginbeom <rydn2002@gmail.com>
2018-07-01 18:39:38 +0900

    review

---
+++ step01_basic/src/test5/tete0701.java
@@ -0,0 +1,25 @@
+//package test5;
+//
+//public class tete0701 {
+//	public String name = "±èÀÎÁ÷";
+//	public String cellPhone = "010-0000-0000";
+//	public int age = 25;
+//	
+//	public class Step03Type {
+//		
+//		tete0701 getIn() {
+//			return new tete0701();
+//		}
+//	}
+//	
+//	void m (tete0701 v) {
+//		System.out.println(v.name);
+//	}
+//
+//	public static void main(String[] args) {
+//		Step03Type s = new Step03Type();
+//		tete0701 v = s.getIn();
+//
+//	}
+//
+//}
