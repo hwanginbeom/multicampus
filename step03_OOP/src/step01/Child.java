850149a6518a8bbcf2ab58fc3a5f085f8ab2d783
hwanginbeom <rydn2002@gmail.com>
2018-07-03 11:10:38 +0900

    casting

--- step03_OOP/src/step01/Child.java
+++ step03_OOP/src/step01/Child.java
@@ -4,13 +4,19 @@ public class Child extends Parent{ // 지금 현재 Object 가 가지고 있는 메소드 11�
 	String job;
 	
 	Child(){
-		super();
+//		super(); // 여기서 부모 측에서 넘어간다. 
+									//Object -> Parent(name,age) ->Child(job) 부모부터 생성
+									//생성자 위치로 이동
+		super("창윤",28);
 		System.out.println("자식의 기본 생성자");
 	}
 
 	public static void main(String[] args) {
 		Child c = new Child();
-
+		System.out.println(c.name);
+		c.name = "상만";
+		System.out.println(c.name);
+		c.printAll();
 	}
 
 }
--- step03_OOP/src/step01/Parent.java
+++ step03_OOP/src/step01/Parent.java
@@ -4,13 +4,13 @@ public class Parent extends Object{ //Object 클래스를 상속하는 parent  Object는 
 	 String name;
 	 int age;
 	
-	public Parent() {
+	public Parent() { //기본 생성자
 		super(); // Object 객체가 생성
 		System.out.println("부모의 기본 생성자");
 	} // name , age 변수 공간이 사용가능하게 heap 객체 내부에 생성
 		//Parent 객체 생성 완료
 	
-	public Parent(String name, int age) {
+	public Parent(String name, int age) { //생성자
 		super();
 		this.name = name;
 		this.age = age;
